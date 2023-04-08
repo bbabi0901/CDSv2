e9212bdc7ff72c46bebea8159f5e2308ef8f26a3
bbabi0901 <69451349+bbabi0901@users.noreply.github.com>
2023-04-08 14:56:07 +0900

    Fix: deploy oracle

---
+++ contract/scripts/2_oracle.js
@@ -0,0 +1,27 @@
+const hre = require('hardhat');
+
+const { writeAddress, isDeployed } = require('./utils');
+
+async function main() {
+  try {
+    let oracleAddr = readAddress('oracle');
+    if (!oracleAddr) {
+      const Oracle = await hre.ethers.getContractFactory('PriceOracleMock');
+      const oracle = await Oracle.deploy();
+      await oracle.deployed();
+
+      oracleAddr = oracle.address;
+      writeAddress('oracle', oracleAddr);
+    }
+    console.log(`Oracle deployed to ${oracleAddr}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
