aed051a8546bd3b229762536c80544164cd73b2d
bbabi0901 <69451349+bbabi0901@users.noreply.github.com>
2023-04-20 17:08:13 +0900

    Fix: ipfs

--- client/src/components/ImageUploader/ImageUploader.tsx
+++ client/src/components/ImageUploader/ImageUploader.tsx
@@ -1,6 +1,14 @@
-// modules
+// react modules
 import React, { useEffect, useState, MouseEvent } from 'react';
-import { Typography, Button } from 'antd';
+
+// antd
+import { Typography, Button, message, Upload } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
+
+// ipfs
+import { create as ipfsHttpClient } from 'ipfs-http-client';
+// import { IpfsImage } from "react-ipfs-image";
 
 // style
 import styled from 'styled-components';
@@ -10,6 +18,7 @@ import { styles } from '../../assets/styles/styles';
 import { ipfsProps } from '../../pages/Mint/Mint';
 
 const { Title, Paragraph } = Typography;
+const { Dragger } = Upload;
 
 const ImageUploader = (props: ipfsProps) => {
   const [isUploaded, setIsUploaded] = useState<boolean>(false);
@@ -30,6 +39,19 @@ const ImageUploader = (props: ipfsProps) => {
         File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG, GLB,
         GLTF. Max size: 100 MB
       </Description>
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibited from
+          uploading company data or other banned files.
+        </p>
+      </Dragger>
+      <br />
       {isUploaded ? (
         <div>
           <Button disabled type="text">
@@ -41,6 +63,26 @@ const ImageUploader = (props: ipfsProps) => {
       )}
     </>
   );
+};
+
+const props: UploadProps = {
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
 };
 
 const Description = styled(Paragraph)`
